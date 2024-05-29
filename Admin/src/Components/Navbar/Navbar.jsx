310f7450f7fdcd8835af57e5105c30a7d85567be
Rasakulendran Sanchayan <121174975+Sanchayan12@users.noreply.github.com>
2024-05-29 15:35:45 +0530

    Add files via upload

---
+++ Admin/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "./Navbar.css";
+import navlogo from "../../assets/nav-logo.svg";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img src={navlogo} alt="" className="nav-logo" />
+    </div>
+  );
+};
+
+export default Navbar;
